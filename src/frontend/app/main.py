98b18d82e18ee0dbe27c529641ae3d3afb41295c
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2023-03-09 16:01:42 +0100

    implemented otel and logging to stdout

--- src/frontend/app/main.py
+++ src/frontend/app/main.py
@@ -1,20 +1,32 @@
 from flask import Flask
 from waitress import serve
 from lorem_text import lorem
-from fluent import sender
-from fluent import event
-sender.setup('fluentd.test')
+from tilt.logging import TiltLogger
+#from tilt.pellucid import Pellucid
 
 app = Flask(__name__)
 
+# Create the tilt Logger
+tl = TiltLogger("TILT")
+
+#pellucid = Pellucid()
+#pellucid.personal_data
+#pellucid.purpose
+#@pellucid.activity_name("Rechnung")
+#@pellucid.data_disclosed(["firstname","name","address","payment_card"])
+#@pellucid.purposes(["Rechnungslegung"])
+#@pellucid.log()
+
+
 @app.route('/')
+@tl.log(concept_name = "HelloWorld", tilt = {"data_disclosed":["username"],"purposes":["marketing"],"legal_bases":["gdpr sec 4."]}, msg = "Logging")
 def hello():
     e = {
         'id': "MYID",
-        'value':  lorem.sentence() 
-        }
-    event.Event('follow', e)
+        'value': lorem.sentence()
+    }
+
     return e
 
 if __name__ == "__main__":
-    serve(app)
+    serve(app, host="0.0.0.0", port=8089)
