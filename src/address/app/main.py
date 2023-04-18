86b346fb4fe51884641bd39bde528cd711eef7c9
ciphersmaug <39407479+ciphersmaug@users.noreply.github.com>
2023-04-18 20:16:38 +0200

    Address service implemented

---
+++ src/address/app/main.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from waitress import serve
+#from process_tilt_ciphersmaug.logging import TiltLogger
+from opentelemetry import trace
+import sqlite3
+
+def initialize_db():
+    db = sqlite3.connect("address.sqlite")
+    cursor = db.cursor()
+    with open("addresses.sql",mode="r") as f:
+        cursor.executescript(f.read())
+    db.close()
+
+tracer = trace.get_tracer(__name__)
+app = Flask(__name__)
+initialize_db()
+# Create the tilt Logger
+#tl = TiltLogger("TILT",tracer)
+@app.route('/address/<id>')
+def address_information(id: int):
+    address = get_address(id)
+    return address
+
+#@tl.log(concept_name = "Combine User Data", tilt = {
+#    "data_disclosed": ["firstname","lastname","birthday","address"], 
+#    "purposes": ["marketing"], 
+#    "legal_bases": ["gdpr sec 6."]
+#    }, msg = "Combine User Data")
+def get_address(id: int):
+    
+    if int(id) > 1000:
+        return {}
+    else:
+        db = sqlite3.connect("address.sqlite")
+        result = {}
+        for row in db.execute(f"SELECT * from addresses WHERE id == {id}"):
+            print(row)
+            id,country,state,city,street,number,postcode = row
+            result["street"]=street
+            result["number"]=number
+            result["postcode"]=postcode
+            break
+        db.close()
+        return {"address":result}
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8083)
